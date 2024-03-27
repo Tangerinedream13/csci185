222bdc18ba98d6961d1343abf9f824573deb2320
Maria <mariabhaddon@gmail.com>
2024-03-27 13:52:45 -0400

    commit

--- lectures/lecture16/03-drawings/sketch.js
+++ lectures/lecture16/03-drawings/sketch.js
@@ -4,6 +4,17 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
     background("pink");
 
+    fill ("black");
+    rect (400,200,700,400);
+    circle (500, 700, 100);
+    fill ("blue");
+    circle (500, 700, 200);
+    fill ("blue");
+    circle (900, 700, 200);
+    fill ("black");
+    rect (200, 400, 200, 200);
+
+
     /* Use the drawing functions to draw a picture. Ideas:
         * Draw an animal
         * Draw a tree
@@ -20,19 +31,20 @@ function setup() {
     rect(30, 20, 55, 55);
 
     // add your drawing here:
-    // https://p5js.org/reference/#/p5/point
-    // point(110, 20);
+    https://p5js.org/reference/#/p5/point
+    strokeWeight (20);
+    point(110, 20);
 
     // https://p5js.org/reference/#/p5/text
-    // text("Here is some text", 200, 40);
+    text("Here is some text", 200, 40);
 
     // https://p5js.org/reference/#/p5/ellipse
-    // fill('red');
-    // ellipse(100, 200, 60, 100);
+    fill('red');
+    ellipse(100, 200, 60, 100);
 
     // Other shapes...
-    // Polygon: https://p5js.org/reference/#/p5/beginShape
-    // Line: https://p5js.org/reference/#/p5/line
+    //Polygon: https://p5js.org/reference/#/p5/beginShape
+    //Line: https://p5js.org/reference/#/p5/line
     
     // Curve: https://p5js.org/reference/#/p5/curve
 
