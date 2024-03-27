7a8bdfc5f45aa3c717824467a8fe60a820ff88e3
Maria <mariabhaddon@gmail.com>
2024-03-26 23:21:09 -0400

    commit

--- lectures/lecture16/02-function-with-parameters/main.js
+++ lectures/lecture16/02-function-with-parameters/main.js
@@ -1,1 +1,21 @@
+function changeColor (color) {
+    const bodyEl = document.querySelector ("body");
+    bodyEl.style.backgroundColor = color;
+}
+
+//function makeRed() {
+    //document.querySelector ("body").style.backgroundColor = "#B23";
+    //bodyEl.style.backgroundColor =
+    //const bodyEl = document.querySelector ("body");
+    //target the body and change its background color to red
+//} 
+//function makeBlue() {
+    //document.querySelector ("body").style.backgroundColor = "#2af";
+//}
+//function makeGreen() {
+    //document.querySelector ("body").style.backgroundColor = "green";
+//}
 // make a function:
+//change to red
+//change to blue
+//change to red
