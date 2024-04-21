05360857170c5bea75d00f7fdc503d044cb8a619
Maria <mariabhaddon@gmail.com>
2024-04-20 22:29:41 -0400

    commit

--- tutorials/tutorial11/exercise04/main.js
+++ tutorials/tutorial11/exercise04/main.js
@@ -23,3 +23,4 @@ for (let i = 0; i < restaurants.length; i++) {
 //The for loop iterates over the restaurants array. Inside the loop, there's a condition that checks if the restaurant's category is "pizza".
 //If the category is "pizza", a new paragraph element (<p>) is created
 //the restaurant's name is set as the text content of the paragraph
+//Chat GPT
