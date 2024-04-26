5435e86c712b1d0fbb7ba4ac435357a06229bcd9
Maria <mariabhaddon@gmail.com>
2024-04-26 12:10:44 -0400

    commit

---
+++ homework/Homework 6/your_task/main.js
@@ -0,0 +1,60 @@
+const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
+
+function search (ev) {
+    const term = document.querySelector('#search').value;
+    console.log('search for:', term);
+    // issue three Spotify queries at once...
+    getTracks(term);
+    getAlbums(term);
+    getArtist(term);
+    if (ev) {
+        ev.preventDefault();
+    }
+}
+
+async function getTracks (term) {
+    console.log(`
+        get tracks from spotify based on the search term
+        "${term}" and load them into the #tracks section 
+        of the DOM...`);
+}
+
+async function getAlbums (term) {
+    console.log(`
+        get albums from spotify based on the search term
+        "${term}" and load them into the #albums section 
+        of the DOM...`);
+}
+
+async function getArtist (term) {
+    const url = `${baseURL}?q=${term}&type=artist&limit=1`;
+    console.log (url);
+    const request = await fetch (url);
+    const data = await request.json();
+    console.log (data);
+    // go to the spotify API with the parameter term
+    // type=artist&limit=1
+    // q=term&type=artist&limit=1
+    // insert it into the DOM
+
+    const snippet = `
+        <div>
+            <img src="https://i.scdn.co/image/0c9057cb30520f9f883a220051260fc66a2f3ffa">
+                <h2>BTS</h2>
+                    <div class="footer">
+                        <a href="https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX" target="_blank">
+                            view on spotify
+                                </a>
+                            </div>
+                        </div>
+                    </section>`;
+}
+
+document.querySelector('#search').onkeyup = function (ev) {
+    // Number 13 is the "Enter" key on the keyboard
+    console.log(ev.keyCode);
+    if (ev.keyCode === 13) {
+        ev.preventDefault();
+        search();
+    }
+}
