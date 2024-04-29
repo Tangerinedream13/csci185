99011d68a2c9e644c65b43939f38e5471f704a17
Maria <mariabhaddon@gmail.com>
2024-04-29 12:11:59 -0400

    commit

---
+++ quizzes/quiz02/exercise01/main.js
@@ -0,0 +1,12 @@
+function changeBackground() {
+    document.querySelector("#s1").style.backgroundColor = "#pink";
+}
+function goodbye() {
+    document.querySelector('button').onclick = function() {
+        this.textContent = 'Goodbye';
+    };
+}
+function showDog() {
+    document.querySelector('image').src = ""`
+    element
+}
