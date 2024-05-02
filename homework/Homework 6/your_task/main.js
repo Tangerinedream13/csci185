8e10a6d9a8f9fe46897d567b47b0292ed45ba3d6
Maria <mariabhaddon@gmail.com>
2024-05-02 17:34:27 -0400

    commit

--- homework/Homework 6/your_task/main.js
+++ homework/Homework 6/your_task/main.js
@@ -3,7 +3,6 @@ const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
 function search (ev) {
     const term = document.querySelector('#search').value;
     console.log('search for:', term);
-    // issue three Spotify queries at once...
     getTracks(term);
     getAlbums(term);
     getArtist(term);
@@ -12,55 +11,83 @@ function search (ev) {
     }
 }
 
-async function getTracks (term) {
-    console.log(
-        const url = `https://www.apitutor.org/spotify/simple/v1/search?type=track&q=${term}`;
-        const response = await fetch (url);
-        const trackData = await response.json();
+function playTrack(trackId) {
+    const template = `
+    <iframe style="border-radius:12px" 
+        src="https://open.spotify.com/embed/track/${trackId}?utm_source=generator" 
+        width="100%" 
+        height="352" 
+        frameBorder="0" 
+        allowfullscreen="" 
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+        loading="lazy">
+    </iframe>
+    `;
 
-        for (let i = 0; 1 < 5; i++) {
-            const track = trackData [i];
-            const template = `
-                <section class="track-item preview">
-                    <img src="${track.album.image_url}">
-                    <i class="fas play-track fa-play" aria-hidden="true"></i>
-                    <div class="label">
-                        <h2>${track.name}</h2>
-                        <p>${track.artist.name}</p>
+    document.querySelector("#artist").innerHTML = template;
+}
+
+async function getTracks (term) {
+    const url = `https://www.apitutor.org/spotify/simple/v1/search?type=track&q=${term}`;
+    const response = await fetch (url);
+    const trackData = await response.json();
+    console.log(trackData);
+    document.querySelector("#tracks").innerHTML = "";
+    for (let i = 0; i < 5; i++) {
+        const track = trackData [i];
+        const template = `
+            <section class="track-item preview" onclick="playTrack('${track.id}')">
+                <img src="${track.album.image_url}">
+                <i class="fas play-track fa-play" aria-hidden="true"></i>
+                <div class="label">
+                    <h2>${track.name}</h2>
+                    <p>${track.artist.name}</p>
             </div>
-            </section>
-            `;
-            document.querySelector("#tracks").innerHTML += template; 
+        </section>
+        `;
+        document.querySelector("#tracks").innerHTML += template; 
     }
 }
 
 async function getAlbums (term) {
-    console.log(
-        const url = `https://www.apitutor.org/spotify/simple/v1/search?type=album&q=${term}`;
-        const response = await fetch (url);
-        const albumData = await response.json();
-        const album = artistAlbum[0];
+    const url = `https://www.apitutor.org/spotify/simple/v1/search?type=album&q=${term}`;
+    const response = await fetch (url);
+    const albumData = await response.json();
+    console.log(albumData);
+    if (albumData.length === 0) {
+        document.querySelector("#albums").innerHTML = "No albums were returned.";
+        return;
+    }
+    document.querySelector("#albums").innerHTML = "";
+    for (let i = 0; i < 4; i++) {
+        const album = albumData [i];
         const template = `
-            <section class="album-card" id="2lATw9ZAVp7ILQcOKPCPqp">
-            <div>
-            <img src="https://i.scdn.co/image/ab67616d0000b2736feb6d9ed7891f40e9a524dd">
-                <h2>Love Yourself 結 'Answer'</h2>
+        <section class="album-card" id="2lATw9ZAVp7ILQcOKPCPqp">
+        <div>
+            <img src="${album.image_url}">
+            <h2>${album.name}</h2>
             <div class="footer">
                 <a href="https://open.spotify.com/album/2lATw9ZAVp7ILQcOKPCPqp" target="_blank">
-                view on spotify
+                    view on spotify
                 </a>
             </div>
-            </div>
-            </section>
-        
-        `
+        </div>
+        </section>
+        `;
+        document.querySelector("#albums").innerHTML += template; 
+    }
+    
 }
 
 async function getArtist (term) {
     const url = `https://www.apitutor.org/spotify/simple/v1/search?type=artist&q=${term}`;
     const response = await fetch (url);
     const artistData = await response.json();
-    const artist = artistData[0];
+    const artist = artistData[0]; 
+    if (!artist) { 
+        document.querySelector("#artist").innerHTML = "No artist has been returned for your search.";
+        return; 
+    }
     const template = `
         <section class="artist-card" id="${artist.id}">
                 <div>
@@ -70,13 +97,12 @@ async function getArtist (term) {
                         <a href="${artist.spotify_url}" target="_blank">
                             view on spotify
                         </a>
+                    </div>
                 </div>
-            </div>
-        </section>
-    `;
+            </section>
+        `;
     document.querySelector("#artist").innerHTML = template; 
 }
-
 
 document.querySelector('#search').onkeyup = function (ev) {
     // Number 13 is the "Enter" key on the keyboard
