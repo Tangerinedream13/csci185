6c519399a3b100aa5482eac082ad775f7e7cd376
Maria <mariabhaddon@gmail.com>
2024-05-03 09:06:02 -0400

    commit

--- quizzes/quiz02/exercise01/main.js
+++ quizzes/quiz02/exercise01/main.js
@@ -1,7 +1,6 @@
 function changeBackground() {
     console.log ("test");
     document.querySelector("#s1").style.backgroundColor = "pink";
-
     // let el = document.querySelector ("#s1");
     // el.style.backgroundColor = "#02afd7";
 }
